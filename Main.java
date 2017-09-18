bcaa71eb65e6d79c06c1b2a3f7d162f490c4580b
csweb2 <webmaster@cs.byu.edu>
2017-09-18 14:56:47 -0600

    corrected input handler

--- Main.java
+++ Main.java
@@ -6,26 +6,41 @@ public class Main {
    * @param args [0-x]dictionary with whitespace, [x + 1]wordLength, [x + 2]guesses
    */
   public static void main (String[] args) {
-    int wordLength, guesses;
-    File dictionary;
+    int wordLength = 0;
+    int guesses = 0;
+    String filePath = "";
+    File dictionary = null;
 
     try {
-      dictionary = new File(args[0]);
-      wordLength = Integer.parseInt(args[1]);
-      guesses = Integer.parseInt(args[2]);
+      for (int i = 0; i < args.length - 2; i++){
+        filePath += args[i];
+      }
+      dictionary = new File(filePath);
+      wordLength = Integer.parseInt(args[args.lenth - 2]);
+      guesses = Integer.parseInt(args[args.lenth - 1]);
+      if (wordLength < 2 || guesses < 1) {
+        uException();
+      }
     }
     catch (NumberFormageException | IndexOutOfBoundsException  e) {
-      System.out.println("Usage: java EvilHangman dictionary wordLength guesses");
-      System.exit(0);
+      uException();
     }
 
-    if (wordLength < 2 || )
-
-    startGame(dictionary, wordLength);
+    EvilHangman hang = new EvilHangman();
+    hang.startGame(dictionary, wordLength);
+    gameLoop(hang, guesses);
 
   }
 
-  gameLoop(dictionary, wordLength, guesses) {
+  public static void uException() {
+    System.out.println("Usage: java hangman.Main dictionary wordLength guesses");
+    System.exit(0);
+  }
 
+  gameLoop(EvilHangman hang, guesses) {
+
+    while (guesses > 0) {
+      guess--;
+    }
   }
 }
